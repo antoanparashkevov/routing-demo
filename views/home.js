663d8cbca7494d6f347e0ff83450b866a71fdf16
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 15:04:32 +0300

    Using lit-html for rendering template

--- views/home.js
+++ views/home.js
@@ -1,5 +1,9 @@
-const main = document.querySelector('main')
+import { html, render } from "../node_modules/lit-html/lit-html.js";
 
-export function showHome( ){
-main.innerHTML = `<h2>Home page</h2>`
+const homeTemplate = () => html` <h2>Home page</h2> `;
+
+const main = document.querySelector("main");
+
+export function showHome() {
+  render(homeTemplate(), main);
 }
