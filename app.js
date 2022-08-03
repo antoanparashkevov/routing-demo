e8e69a7e938d9c2282df6efee8d922b22628d72d
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 15:00:42 +0300

    export function which is responsible for home page

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import page from './node_modules/page/page.mjs'
+import { showHome } from './views/home.js';
 
 const main = document.querySelector('main')
 
 page('/','/home');
-page('/home',() => main.innerHTML = `<h2>Home page</h2>`);
+
+page('/home',showHome);
 page('/catalog',() => main.innerHTML = `<h2>Catalog page</h2>`);
 page('/about',() => main.innerHTML =`<h2>About page</h2>`);
 
---
+++ views/home.js
@@ -0,0 +1,5 @@
+const main = document.querySelector('main')
+
+export function showHome( ){
+main.innerHTML = `<h2>Home page</h2>`
+}
