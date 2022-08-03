76fbb1a901ba5e9bf28ad2bd66f534dd8d4947cc
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 14:55:13 +0300

    Using alias to redirect to home page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import page from './node_modules/page/page.mjs'
 
 const main = document.querySelector('main')
 
-page('/',() => main.innerHTML = `<h2>Home page</h2>`);
+page('/','/home');
 page('/home',() => main.innerHTML = `<h2>Home page</h2>`);
 page('/catalog',() => main.innerHTML = `<h2>Catalog page</h2>`);
 page('/about',() => main.innerHTML =`<h2>About page</h2>`);
