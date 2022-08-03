5d2feceabc69df60a9d258c9d1d427f3329c8f20
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 15:37:46 +0300

    Using decorator function to add behaviour to the ctx

--- app.js
+++ app.js
@@ -8,25 +8,22 @@ import { showCatalog } from "./views/catalog.js";
 const main = document.querySelector("main");
 
 page("/", "/home");
-page("/home", (ctx) =>{
-    ctx.render = render;
-    showHome(ctx)});
-page("/catalog", (ctx) => {
-    ctx.render = render;
-    showCatalog(ctx)
-});
+page("/home", (ctx) => decorateContext(ctx, showHome));
+page("/catalog", (ctx) => decorateContext(ctx, showCatalog));
 // using dynamic params
 page("/catalog/:id", showDetails);
-page("/about", (ctx) => {
-    ctx.render = render;
-    showAbout(ctx)
-});
+page("/about", (ctx) => decorateContext(ctx, showAbout));
 
 page.start();
 
 function render(templateResult) {
   litRender(templateResult, main);
 }
-function showDetails(ctx){
-    console.log(ctx.params.id)
+function showDetails(ctx) {
+  console.log(ctx.params.id);
+}
+
+function decorateContext(ctx, view) {
+  ctx.render = render;
+  view(ctx);
 }
