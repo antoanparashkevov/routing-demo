505c511fc52800d21d60081c3ed826ab6f5789e6
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 14:04:57 +0300

    comment unused code

--- hashRouter.js
+++ hashRouter.js
@@ -1,16 +1,16 @@
-export function createHashRouter(main, views) {
-    window.addEventListener('hashchange', onChange)
+// export function createHashRouter(main, views) {
+//     window.addEventListener('hashchange', onChange)
 
-    return onChange;
+//     return onChange;
 
-    function onChange() {
-        const hash = window.location.hash
-        const view = views[hash]
+//     function onChange() {
+//         const hash = window.location.hash
+//         const view = views[hash]
 
-        if (typeof view === 'function') {
-            main.innerHTML = view();
-        }
-    }
+//         if (typeof view === 'function') {
+//             main.innerHTML = view();
+//         }
+//     }
 
 
-}
+// }
