689fdc0f1ff23f3d375cd2056fa82953b029c2ff
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 14:48:44 +0300

    add external library called page for routing

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// import {createHashRouter} from "./hashRouter.js";
-import {createHistoryRouter} from "./historyRouter.js";
-
-const main = document.querySelector('main')
 
 const views = {
     '/': () => `<h2>Home page</h2>`,
@@ -9,11 +5,5 @@ const views = {
     '/catalog': () => `<h2>Catalog page</h2>`,
     '/about': () => `<h2>About page</h2>`
 }
-
-// const start = createHashRouter(main, views)
-const start = createHistoryRouter(main,views)
-//Start Application
-
-start()
-
+const main = document.querySelector('main')
 
---
+++ app1.js
@@ -0,0 +1,19 @@
+// import {createHashRouter} from "./hashRouter.js";
+import {createHistoryRouter} from "./historyRouter.js";
+
+const main = document.querySelector('main')
+
+const views = {
+    '/': () => `<h2>Home page</h2>`,
+    '/home': () => `<h2>Home page</h2>`,
+    '/catalog': () => `<h2>Catalog page</h2>`,
+    '/about': () => `<h2>About page</h2>`
+}
+
+// const start = createHashRouter(main, views)
+const start = createHistoryRouter(main,views)
+//Start Application
+
+start()
+
+
