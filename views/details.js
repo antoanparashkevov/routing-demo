65f58824bf1575df001c9fe84d7516559e93c571
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 15:51:33 +0300

    Show dynamically product id

--- views/details.js
+++ views/details.js
@@ -1,12 +1,13 @@
 import { html } from "../node_modules/lit-html/lit-html.js";
 
-const detailsTemplate = () => html
+const detailsTemplate = (id) => html
 `
 <h1>Details</h1>
-<p>Details for product</p>
+<p>Details for ${id}</p>
 `
 
 export function showDetails(ctx) {
+    const productId = ctx.params.id
     console.log(ctx.params.id);
-    ctx.render(detailsTemplate())
+    ctx.render(detailsTemplate(productId));
   }
