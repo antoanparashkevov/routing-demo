2f9cc63c701ba31f7af3ed25965feb250e72cb7c
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 14:05:43 +0300

    prevent browser default behavior and change content relative to the pathname

--- historyRouter.js
+++ historyRouter.js
@@ -1,13 +1,22 @@
-export function createHistoryRouter(main,views){
-    // window.addEventListener('popstate',onChange)
-    return onChange;
-
-    function onChange(){
-        const pathname = window.location.pathname
-
-        const view = views[pathname];
-        if(typeof view === 'function'){
-            main.innerHTML = view()
-        }
+export function createHistoryRouter(main, views) {
+  window.addEventListener("popstate", onChange);
+  document.body.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") {
+      event.preventDefault();
+      window.history.pushState(null,'',event.target.href)
+      onChange();
     }
+  });
+  return onChange;
+
+  function onChange() {
+    const pathname = window.location.pathname;
+    // console.log(pathname);
+
+    const view = views[pathname];
+
+    if (typeof view === "function") {
+      main.innerHTML = view();
+    }
+  }
 }
