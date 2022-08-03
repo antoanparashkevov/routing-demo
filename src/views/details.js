84357572ef323f3dc54fed0157baefd23bac7fbc
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-03 16:52:54 +0300

    Dynamically load each recipe

--- src/views/details.js
+++ src/views/details.js
@@ -1,16 +1,21 @@
 import { html } from "../../node_modules/lit-html/lit-html.js";
 import { getRecipeById } from "../data/recipes.js";
 
-const detailsTemplate = (id) => html`
+const detailsTemplate = (recipe) => html`
   <h1>Details</h1>
-  <p>Details for ${id}</p>
+  ${recipe ? html`
+  <h2>${recipe.name}</h2>
+ ${recipe.ingredients.map(i=>html`<p>${i}</p>`)}
+ ${recipe.steps.map(s=>html`<p>${s}</p>`)}
+
+  ` : html`Loading...`}
 `;
 
 export async function showDetails(ctx) {
   const productId = ctx.params.id;
   console.log(ctx.params.id);
-  ctx.render(detailsTemplate(productId));
+  ctx.render(detailsTemplate());
 
   const recipe = await getRecipeById(productId);
-  ctx.render(detailsTemplate(recipe.name));
+  ctx.render(detailsTemplate(recipe));
 }
