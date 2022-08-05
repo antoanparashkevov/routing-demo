f0b5923cd416d3a20d4db98504e9d4736356be57
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-05 16:03:01 +0300

    Combination of until directive to wait until the promise to be resolved

--- src/views/catalog.js
+++ src/views/catalog.js
@@ -1,13 +1,14 @@
 import {html, nothing} from "../../node_modules/lit-html/lit-html.js";
+import {until} from '../../node_modules/lit-html/directives/until.js'
 import {repeat} from "../../node_modules/lit-html/directives/repeat.js";
 import {getAllCars} from "../data/cars.js";
 import {createSubmitHandler} from "../data/util.js";
 
-const catalogTemplate = (cars,searcher, pager) => html`
+const catalogTemplate = (cars, searcher, pager) => html`
     <h2>Catalog page</h2>
     ${searcher}
     ${pager}
-  
+
     ${repeat(cars, (car) => car._id, carCard)}
 `;
 
@@ -26,29 +27,35 @@ const pagerTemplate = (page, pages) => html
         </div>`
 
 
-const searchTemplate = (search,onSubmit)=>html
-`<form @submit="${onSubmit}">
-  <label for="search">Search: <input type="text" name="search" .value="${search}"></label>
-  <input type='submit' value="Search">
-  
-</form>`
+const searchTemplate = (search, onSubmit) => html
+    `
+        <form @submit="${onSubmit}">
+            <label for="search">Search: <input type="text" name="search" .value="${search}"></label>
+            <input type='submit' value="Search">
+
+        </form>`
 
 export async function showCatalog(ctx) {
     // displaying query string into browser console.
     // console.log(ctx.querystring)
     // console.log(ctx.query)
+    ctx.render(until(catalogWrapper(ctx), 'Loading...'))
 
+
+}
+
+async function catalogWrapper(ctx) {
+    const search = Number(ctx.query.search) || ''
     //if we don't have any page, this will return undefined, but undefined is falsy value and will display 1
     const page = Number(ctx.query.page) || 1
-  const search = Number(ctx.query.search) || ''
 
     //render empty string while we wait for response
     ctx.render(catalogTemplate([], page));
 
     const {data, pages} = await getAllCars(page);
-    ctx.render(catalogTemplate(data,searchTemplate(search,createSubmitHandler(onSubmit)), pagerTemplate(page, pages)));
+    return catalogTemplate(data, searchTemplate(search, createSubmitHandler(onSubmit)), pagerTemplate(page, pages));
 
-    function onSubmit(data){
-    ctx.page.redirect('/catalog/?search=' + data.search)
+    function onSubmit(data) {
+        ctx.page.redirect('/catalog?search=' + data.search)
     }
 }
