acced88316588149647224d482b2500a087182ef
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-05 14:48:58 +0300

    Calculate offset for getting correct count of data from server

--- src/data/cars.js
+++ src/data/cars.js
@@ -1,14 +1,15 @@
 import * as api from "./api.js";
 
+const pageSize = 4;
 const endpoints = {
-  cars: "/data/cars",
-  carById:'/data/cars/'
+    cars: `/data/cars?pageSize=${pageSize}&offset=`,
+    carById: '/data/cars/'
 };
 
-export async function getAllCars() {
-  return api.get(endpoints.cars);
+export async function getAllCars(page) {
+    return api.get(endpoints.cars + (page - 1) * pageSize);
 }
 
-export async function getCarById(id){
+export async function getCarById(id) {
     return api.get(endpoints.carById + id);
 }
--- src/views/catalog.js
+++ src/views/catalog.js
@@ -14,16 +14,16 @@ const catalogTemplate = (cars,page) => html`
 const carCard = (car) => html`<li><a href="/catalog/${car._id}">${car.make} ${car.model}</a></li>`;
 
 export async function showCatalog(ctx) {
-  //displaying query string into browser console.
+  // displaying query string into browser console.
   // console.log(ctx.querystring)
-  
+  // console.log(ctx.query)
+
   //if we don't have any page, this will return undefined, but undefined is falsy value and will display 1
   const page = Number(ctx.query.page) || 1
-  console.log(ctx.query.page)
 
   //render empty string while we wait for response
   ctx.render(catalogTemplate([],page));
 
-  const cars = await getAllCars();
+  const cars = await getAllCars(page);
   ctx.render(catalogTemplate(cars,page));
 }
