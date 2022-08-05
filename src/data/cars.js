dba0fe6221c8b58b3c391ed7ceb302f891ff2064
“antoanparashkevov” <tonkata1505@gmail.com>
2022-08-05 15:03:25 +0300

    Parallel send two promises. One for size of the cars list, the other for pagination

--- src/data/cars.js
+++ src/data/cars.js
@@ -3,11 +3,19 @@ import * as api from "./api.js";
 const pageSize = 4;
 const endpoints = {
     cars: `/data/cars?pageSize=${pageSize}&offset=`,
+    size:'/data/cars?count',
     carById: '/data/cars/'
 };
 
 export async function getAllCars(page) {
-    return api.get(endpoints.cars + (page - 1) * pageSize);
+    const [data,size] = Promise.all([
+        api.get(endpoints.cars + (page - 1) * pageSize),
+        api.get(endpoints.size),
+    ])
+    return {
+        data,
+        size
+    }
 }
 
 export async function getCarById(id) {
